12eeff5018ca747d002e294a23694e9b9c3efe31
Aswathy Mohan <31227454+Aswathy1891989@users.noreply.github.com>
2017-11-21 12:05:32 -0500

    First Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,10 @@
+import os
+import csv
 
+filePath=os.path.join("raw_data","election_data_1.csv")
+
+with open(filePath) as fileOpen:
+	readFile=csv.reader(fileOpen,delimiter=',')
+	#print(readFile)
+	for row in readFile:
+		print(row)
