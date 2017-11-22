5840acd86e4068ce4cf7f4ae417145bc143ca002
Aswathy Mohan <31227454+Aswathy1891989@users.noreply.github.com>
2017-11-22 09:32:45 -0500

    Update 1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,21 +1,34 @@
 import os
 import csv
 import operator
+import collections
 
 filePath=os.path.join("raw_data","election_data_1.csv")
-count=0
-with open(filePath) as fileOpen:
-	readFile=csv.reader(fileOpen,delimiter=',')
-	next(readFile)
-	sortCSV=sorted(readFile,key=operator.itemgetter(2))
-	
-	#print(readFile)
-	for row in sortCSV:
-		
-		if row[2]!=next(readFile).row[2]:
-			print(count)
-			count=0
-		else:
-			count+=1
+Winner=0
 
-#print(count)
+with open(filePath) as fileOpen:
+	readFile1=csv.reader(fileOpen,delimiter=',')
+#	readFile2=csv.reader(fileOpen,delimiter=',')
+	
+	next(readFile1)
+
+	counts = collections.Counter()
+	for row in readFile1:
+		counts[row[2]]+=1
+with open(filePath) as fileOpen:
+	readFile1=csv.reader(fileOpen,delimiter="'")
+	totalVotes=len(list(readFile1))
+    	
+print("Election Results")		
+print("--------------------------------")
+print("Total Votes: ",totalVotes)
+print("--------------------------------")
+for showItems,showKeys in counts.items():
+	percentageVote=int((showKeys/totalVotes)*100)
+	print(showItems, " : " ,percentageVote,"%  (",showKeys,")")
+	if showKeys>Winner:
+		Winner=showKeys
+		winnerName=showItems
+print("--------------------------------")
+print("Winner: ",winnerName)
+print("--------------------------------")
