e31ed87867e448fc2f3a4380558cfcd5ca987d32
Aswathy Mohan <31227454+Aswathy1891989@users.noreply.github.com>
2017-11-21 19:03:12 -0500

    Update PYPOLL

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,21 @@
 import os
 import csv
+import operator
 
 filePath=os.path.join("raw_data","election_data_1.csv")
-
+count=0
 with open(filePath) as fileOpen:
 	readFile=csv.reader(fileOpen,delimiter=',')
+	next(readFile)
+	sortCSV=sorted(readFile,key=operator.itemgetter(2))
+	
 	#print(readFile)
-	for row in readFile:
-		print(row)
+	for row in sortCSV:
+		
+		if row[2]!=next(readFile).row[2]:
+			print(count)
+			count=0
+		else:
+			count+=1
+
+#print(count)
