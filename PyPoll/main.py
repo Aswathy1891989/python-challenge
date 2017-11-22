8d6f5f3135b6a9857aeec1935955a074e2284db9
Aswathy Mohan <31227454+Aswathy1891989@users.noreply.github.com>
2017-11-22 13:04:11 -0500

    update  textFile

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,11 +24,26 @@ print("--------------------------------")
 print("Total Votes: ",totalVotes)
 print("--------------------------------")
 for showItems,showKeys in counts.items():
-	percentageVote=int((showKeys/totalVotes)*100)
-	print(showItems, " : " ,percentageVote,"%  (",showKeys,")")
+	percentageVote=(showKeys/totalVotes)*100
+	print(showItems, " : ","%.1f" %percentageVote,"%  (",showKeys,")")
 	if showKeys>Winner:
 		Winner=showKeys
 		winnerName=showItems
 print("--------------------------------")
 print("Winner: ",winnerName)
 print("--------------------------------")
+fileWritePath=os.path.join("raw_data","Election_Report.txt")
+fileWriter=open(fileWritePath,"w")
+fileWriter.write("Election Results\n")
+fileWriter.write("--------------------------------\n")
+fileWriter.writelines("Total Votes: "+str(totalVotes)+"\n")
+fileWriter.write("--------------------------------\n")
+for showItems,showKeys in counts.items():
+	percentageVote=(showKeys/totalVotes)*100
+	fileWriter.writelines(showItems + " : "+"%.1f" %percentageVote +"%  ("+str(showKeys)+")")
+	fileWriter.write("\n")
+fileWriter.write("--------------------------------\n")
+fileWriter.write("Winner: "+winnerName)
+fileWriter.write("\n")
+fileWriter.write("--------------------------------\n")
+fileWriter.close()
